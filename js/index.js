47edc464a471aabfcd3e0da2018055b45a35a443
Gaykheshubhangi <shubhangi1819@gmail.com>
2020-12-10 12:23:53 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+console.time('your code took');
+console.log("hello console");
+
+console.log(4*34);
+console.log(true)
+
+console.log([34,2,1]);
+console.log({harry: 'this,marks:34'});
+console.table({harry:'this',marks:34});
+console.warn("this is warning");
+//console.clear();
+console.timeEnd('your code took');
+//console.assert(566<189,'age>189 this is not possible')
+console.error('this is an error')
